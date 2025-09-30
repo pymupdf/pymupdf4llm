0aa768b9febb40e9f99af85fbaf2e825203cc0d9
creathrow <creathrow@gmail.com>
2025-09-30 20:50:42 +0900

    First commit: Contents of the pymupdf-layout-250930 package.
    Due to concerns about dependencies, I intentionally do not commit test code.
    It can be used as follows.
    
    -------------------------------------------------------------
    import layout.DocumentLayoutAnalyzer as DocumentLayoutAnalyzer
    from layout.pymupdf_util import create_input_data_from_page
    
    doc = pymupdf.open(pdf_path)
    da = DocumentLayoutAnalyzer.get_model()
    
    for page in doc:
    	data_dict = create_input_data_from_page(page)
    	det_result = da.predict(data_dict)
    ------------------------------------------------------------
    
    Also, since I didn't discuss it yet, I intentionally avoided modifying pyproject.toml. The following dependency packages are required to run the committed code.
    
    ------------------------------------------------------------
    dependencies = [
        "pyyaml",
        "numpy",
        "onnxruntime",
        "networkx",
    ]
    ------------------------------------------------------------

---
+++ source/layout/__init__.py
@@ -0,0 +1,20 @@
+import sys
+
+if "pymupdf" not in sys.modules:
+    raise ImportError("pymupdf must be imported before importing this module.")
+
+def activate():
+    """Create a layout analyzer function using an ONNX model."""
+    import pymupdf
+    from . import DocumentLayoutAnalyzer
+    from . import onnx
+    CU = onnx.common_util
+    MODEL = DocumentLayoutAnalyzer.get_model()
+
+    def _get_layout(*args, **kwargs):
+        page=args[0]
+        data_dict=CU.create_input_data_by_pymupdf(page)
+        det_result = MODEL.predict(data_dict)
+        page.layout_information = det_result
+
+    pymupdf._get_layout = _get_layout
