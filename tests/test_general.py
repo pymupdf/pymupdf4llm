089ea5b1c4e859e54dfca7116c2e0472e2cd4680
Julian Smith <julian.smith@artifex.com>
2025-10-01 12:05:58 +0100

    tests/test_general.py: add test call of pymupdf.layout.activate().
    Shows up various import errors.

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,5 +1,10 @@
 import pymupdf.features
+import pymupdf.layout
+
 
 def test_simple():
     l = pymupdf.features.features_test('foo')
     assert l == 3
+
+def test_activate():
+    pymupdf.layout.activate()
