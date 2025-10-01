ed83c1dfe8ecb197c403c3d7457921db8884797d
Julian Smith <julian.smith@artifex.com>
2025-10-01 16:11:19 +0100

    tests/test_general.py: added test_features().
    Does basic test of Python wrapper of fz_features_for_region().

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,3 +1,4 @@
+import pymupdf
 import pymupdf.features
 import pymupdf.layout
 
@@ -8,3 +9,14 @@ def test_simple():
 
 def test_activate():
     pymupdf.layout.activate()
+
+def test_features():
+    print()
+    rect = pymupdf.mupdf.FzRect(pymupdf.mupdf.FzRect.Fixed_INFINITE)
+    stext_page = pymupdf.mupdf.FzStextPage(rect)    # mediabox
+    region = pymupdf.mupdf.FzRect(0, 0, 100, 100)
+    features = pymupdf.features.fz_features_for_region(stext_page, region, 0)
+    print(f'{features=}:')
+    for name in dir(features):
+        if not name.startswith('_') and name != 'this':
+            print(f'    {name}: {getattr(features, name)!r}')
