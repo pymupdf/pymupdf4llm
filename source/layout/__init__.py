d0a983bf477179846ba1793d4780a80ecc75ca96
Julian Smith <julian.smith@artifex.com>
2025-10-01 12:39:43 +0100

    source/layout/__init__.py: simplified imports.

--- source/layout/__init__.py
+++ source/layout/__init__.py
@@ -1,20 +1,16 @@
-import sys
+from . import DocumentLayoutAnalyzer
+from . import onnx
+from . import common_util
 
-if "pymupdf" not in sys.modules:
-    raise ImportError("pymupdf must be imported before importing this module.")
+import pymupdf
 
 def activate():
     """Create a layout analyzer function using an ONNX model."""
-    import pymupdf
-    from . import DocumentLayoutAnalyzer
-    from . import onnx
-    from . import common_util
-    CU = common_util
     MODEL = DocumentLayoutAnalyzer.get_model()
 
     def _get_layout(*args, **kwargs):
         page=args[0]
-        data_dict=CU.create_input_data_by_pymupdf(page)
+        data_dict=common_util.create_input_data_by_pymupdf(page)
         det_result = MODEL.predict(data_dict)
         page.layout_information = det_result
 
