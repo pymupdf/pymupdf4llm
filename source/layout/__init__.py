c3781e2203b1119b2651bf2a954c08e909dc8759
Jorj X. McKie <jorj.x.mckie@outlook.de>
2025-10-05 06:06:02 -0400

    PyMuPDF interface adjustments
    adjust pymupdf interface
    
    PyMuPDF must use function create_input_data_from_page() from pymupdf_util.
    
    The created _get_layout() interface function should *return* the layout results and not directly modify the PyMuPDF Page object.
    
    I think we can relax the condition for exact equality of PyMuPDF version.

--- source/layout/__init__.py
+++ source/layout/__init__.py
@@ -1,6 +1,6 @@
 from . import DocumentLayoutAnalyzer
 from . import onnx
-from . import common_util
+from . import pymupdf_util
 
 import pymupdf
 
@@ -10,8 +10,8 @@ def activate():
 
     def _get_layout(*args, **kwargs):
         page=args[0]
-        data_dict=common_util.create_input_data_by_pymupdf(page)
+        data_dict=pymupdf_util.create_input_data_from_page(page)
         det_result = MODEL.predict(data_dict)
-        page.layout_information = det_result
+        return det_result
 
     pymupdf._get_layout = _get_layout
