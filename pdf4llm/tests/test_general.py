6ed38242768512413b192e725bfd81602529d55e
Julian Smith <julian.smith@artifex.com>
2026-03-13 14:08:49 +0000

    pdf4llm/tests/test_general.py: new, basic test of conversion to md.
    On windows we need to protect against encoding errors in the tests. And the
    generated markdown is slightly different from expected, which we currently
    ignore.

--- pdf4llm/tests/test_general.py
+++ pdf4llm/tests/test_general.py
@@ -1,21 +1,25 @@
 import difflib
 import os
+
+import pdf4llm
+import pymupdf
 import platform
 
-import pymupdf4llm
-import pymupdf
 
-
-def test_370():
-    path = os.path.normpath(f'{__file__}/../../tests/test_370.pdf')
-    path_expected = os.path.normpath(f'{__file__}/../../tests/test_370_expected.md')
-    path_actual = os.path.normpath(f'{__file__}/../../tests/test_370_actual.md')
+def test_4llm_370():
+    # This is a copy of pymupdf4llm/tests/test_370.py:test_370(), except that
+    # we use pdf4llm.to_markdown() instead of pymupdf4llm.to_markdown(). We
+    # reuse that test's input and expected files.
+    #
+    path = os.path.normpath(f'{__file__}/../../../tests/test_370.pdf')
+    path_expected = os.path.normpath(f'{__file__}/../../../tests/test_370_expected.md')
+    path_actual = os.path.normpath(f'{__file__}/../../../pdf4llm/tests/test_4llm_370_actual.md')
     
     with open(path_expected) as f:
         expected = f.read()
     
     with pymupdf.open(path) as document:
-        actual = pymupdf4llm.to_markdown(
+        actual = pdf4llm.to_markdown(
                 document,
                 write_images=False,  # do not write image files
                 embed_images=False,  # embed images as base64 strings
