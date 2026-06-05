bcda57c03049923e8f29a34798d2326e302d9a79
Jorj X. McKie <jorj.x.mckie@outlook.de>
2026-06-05 16:35:39 -0400

    Remove MD Conversion to PDF

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ else:
         f"pymupdf=={pymupdf_version}",
         f"pymupdf_layout=={pymupdf_layout_version}",
         "tabulate",
-        "markdown",
-        "pymdown-extensions",
     ]
 
 
